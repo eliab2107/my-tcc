c2062d992c31776a63e86c333688c87e90a37f91
Eliab Bernardino <eliab2107@gmail.com>
2026-04-16 23:41:36 -0300

    evolução do consumer e manager

--- Consumer/consumer.py
+++ Consumer/consumer.py
@@ -2,11 +2,13 @@ from dotenv import load_dotenv
 import pika
 import json
 import time
+import requests
+import math
 from threading import Thread, Lock
 import os
 load_dotenv()
 class Consumer:
-    def __init__(self, id:int, prefetch_count=1):
+    def __init__(self, id:int, prefetch_count=1, task_level=1):
         self.host = os.getenv("RABBITMQ_HOST")
         self.user = os.getenv("RABBITMQ_USER")
         self.password = os.getenv("RABBITMQ_PASS")
@@ -25,6 +27,8 @@ class Consumer:
         self.lock = Lock() 
         self.prefetch_count = prefetch_count
         self.channel.basic_qos(prefetch_count=self.prefetch_count)
+        self.channel.basic_consume(queue="fila_teste", on_message_callback=self.callback, auto_ack=False)
+        self.task_level = task_level
 
 
     def callback(self, ch, method, properties, body):
@@ -34,19 +38,19 @@ class Consumer:
         ch.basic_ack(delivery_tag=method.delivery_tag)
         end = time.monotonic()
         with self.lock:
-            self.messsages_metrics.append([end - start, message.get("date_created"), time.time()])
-            for m in self.messsages_metrics:
-                print(f"Consumer {self.id} - Tempo de processamento: {m[0]:.4f} segundos - Tempo na fila: {m[1] - message.get('date_created', time.time()):.4f} segundos")
+            dados = [end - start, time.time() - message.get("date_created")]
+            self.messsages_metrics.append(dados)
                 
     def start_consuming(self)-> None:
-        self.channel.basic_consume(
-            queue="fila_teste",
-            on_message_callback=self.callback,
-            auto_ack=False
-        )
         print("[CONSUMER] Consumindo mensagens...")
-        self.channel.start_consuming()
-        
+        return self.channel.start_consuming()
+    
+    
+    def get_data(self):
+        with self.lock:
+            matriz = self.messsages_metrics.copy()
+            self.messsages_metrics.clear()
+            return matriz    
     
     def set_new_prefetch_count(self, new_prefetch_vount:int)-> None:
         with self.lock:
@@ -55,8 +59,14 @@ class Consumer:
 
 
     def process_message(self, data):
-        time.sleep(0.001)
-        return 1373737/3
+        network_url = "https://www.google.com"
+        try:
+           # r = requests.get(network_url, timeout=10)
+            for _ in range(self.task_level * 1_000):
+                _ = math.sqrt(12345.6789) * math.sin(0.5)
+        except Exception as e:
+            print(f"<- Erro de rede: {e}") 
+        
     
             
 if __name__ == "__main__":
--- Consumer/manager.py
+++ Consumer/manager.py
@@ -1,31 +1,33 @@
 import time
 from threading import Thread
-from execution.clients.consumer import Consumer   
-from typing import List, Dict
-from DTOs.decision import Decision
+from Consumer import consumer
+from typing import List
+import psutil
+import os
 
 class ConsumerManager:
     def __init__(self, num_consumers: int = 1, monitor_interval: float = 1.0):
         self.num_consumers = num_consumers
         self.monitor_interval = monitor_interval
-        self.consumers: List[Consumer] = []
+        self.consumers: List[consumer.Consumer] = []
         self.consumer_threads:List[Thread] = []
         self.monitor_thread = None
         self.running = False
+        self.processo = psutil.Process(os.getpid())
 
 
     def start_consumers(self):
         self.running = True
 
         for i in range(self.num_consumers):
-            consumer = Consumer(id=i)
+            consumidor = consumer.Consumer(id=i, prefetch_count=11, task_level=1)
             thread = Thread(
-                target=consumer.start_consuming,
+                target=consumidor.start_consuming,
                 daemon=True,
                 name=f"consumer-thread-{i}"
-            )
-
-            self.consumers.append(consumer)
+            ) 
+        
+            self.consumers.append(consumidor)
             self.consumer_threads.append(thread)
 
             thread.start()
@@ -42,13 +44,23 @@ class ConsumerManager:
     def monitor_loop(self):
         while self.running:
             time.sleep(self.monitor_interval)
-            self.snapshot_metrics()
+            dados = self.consumers[0].get_data()
+            qtd_mensagens = len(dados)
+            avg_processamento = sum([d[0] for d in dados])/qtd_mensagens if qtd_mensagens > 0 else 0
+            avg_time_in_queue = sum([d[1] for d in dados])/qtd_mensagens if qtd_mensagens > 0 else 0
+            pc_cpu , pc_ram, process_cpu, process_ram = self.obter_consumo_recursos()
+            print(f"\n Quantidade de Mensagens: {qtd_mensagens} \n Average Processing Time: {avg_processamento} \n Average Time in Queue: {avg_time_in_queue} \n CPU Global: {pc_cpu}% \n RAM Global: {pc_ram}% \n CPU Processo: {process_cpu}% \n RAM Processo: {process_ram:.2f} MB\n")
+            #self.snapshot_metrics()
 
 
     def snapshot_metrics(self)->dict:
         metrics = {}
         for consumer in self.consumers:
-            metrics[consumer.id] = consumer.get_metrics()
+            if consumer.id not in metrics:
+                metrics[consumer.id] = []
+            dados = consumer.get_data()
+            print(f"Snapshot Consumer {consumer.id}: {dados}")
+            metrics[consumer.id].append(dados)
 
         return metrics
         
@@ -65,16 +77,34 @@ class ConsumerManager:
     def get_consumers(self):
         return self.consumers
 
+    
+    def obter_consumo_recursos(self):
+        # 1. Consumo GLOBAL da Máquina (CPU e RAM)
+        cpu_global = psutil.cpu_percent(interval=None) # % de uso de todos os núcleos
+        ram_global = psutil.virtual_memory().percent    # % de uso da RAM total
 
-    def update_prefetchs(self, decisions:List[Decision])-> None:
-        for decision in decisions:
-            consumer = self.consumers[decision.consumer_id]
-            with consumer.comands_lock:
-                consumer.comands.append(decision)
+        # 2. Consumo do PROCESSO ATUAL (este script)
+      
+        
+        # O cpu_percent do processo requer um pequeno intervalo ou chamada dupla
+        # Na primeira chamada ele retorna 0.0, nas seguintes a variação desde a última
+        cpu_processo = self.processo.cpu_percent(interval=None)
+        
+        # Memória em MB (RSS = Resident Set Size)
+        ram_processo_mb = self.processo.memory_info().rss / (1024 * 1024)
+
+        return cpu_global,  ram_global, cpu_processo,  ram_processo_mb
+        
+
+    #def update_prefetchs(self, decisions:List[Decision])-> None:
+     #   for decision in decisions:
+      #      consumer = self.consumers[decision.consumer_id]
+       #     with consumer.comands_lock:
+        #        consumer.comands.append(decision)
 
 
 if __name__ == "__main__":
-    manager = ConsumerManager(num_consumers=3)
+    manager = ConsumerManager(num_consumers=1, monitor_interval=5)
     manager.start_consumers()
 
     try:
